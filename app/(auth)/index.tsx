513afaf3007d3fc6feb42af22e9f304f0823e880
Hai Phan <27phanh@gmail.com>
2025-04-18 09:35:24 -0700

    test

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,16 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Link, useRouter } from 'expo-router';
 
 export default function () {
   const router = useRouter();
   return (
     <View className="flex-1 items-center justify-center bg-black">
+      <TextInput
+        placeholder='email'
+        className='bg-white'
+        >
+        
+      </TextInput>
       <TouchableOpacity
         className="bg-white p-4 py-2 rounded-lg"
         onPress={() => router.push('/(tabs)')}
