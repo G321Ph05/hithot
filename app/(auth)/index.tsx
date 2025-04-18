b437778655805146ade81352c60adbc5563b9c90
Hai Phan <27phanh@gmail.com>
2025-04-18 09:35:54 -0700

    news

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -5,14 +5,8 @@ export default function () {
   const router = useRouter();
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <TextInput
-        placeholder='email'
-        className='bg-white'
-        >
-        
-      </TextInput>
       <TouchableOpacity
-        className="bg-white p-4 py-2 rounded-lg"
+        className="bg-white d p-4 py-2 rounded-lg"
         onPress={() => router.push('/(tabs)')}
       >
         <Text className="text-black font-bold text-2xl">Login</Text>
--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-white font-bold text-2xl">Friends</Text>
     </View>
   );
